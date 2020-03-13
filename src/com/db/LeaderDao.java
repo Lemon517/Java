2debcf960b2b5847c47d15ecf0f5e3102d812fa4
Lemon517 <1528052352@qq.com>
2020-03-13 17:44:23 +0800

    第三次上传基本逻辑结构

---
+++ src/com/db/LeaderDao.java
@@ -0,0 +1,78 @@
+package com.db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class LeaderDao {
+    Db db;
+    ResultSet rs;
+
+    public LeaderDao() {
+        db = new Db();
+        
+    }
+
+    private String searchMassage(String name) {
+        String attribute = null;
+        String sql = String.format("select * from user where name='%s'", name);
+        rs = db.selectSqlDate(sql);
+        try {
+            while (rs.next()) {
+            	attribute = rs.getString("attribute");
+            }
+            return attribute;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        
+
+    }
+
+    private String[][] ArrayListToArray(ArrayList<ArrayList<String>> data){
+        String[][] allData = new String[data.size()][20];
+        for (int i=0;i<data.size();i++){
+            ArrayList<String> al = data.get(i);
+            allData[i][0] = String.valueOf(i+1);
+            for(int j=0;j<al.size();j++){
+                allData[i][j+1]=al.get(j);
+            }
+        }
+        return allData;
+    }
+
+    private String getYesNo(String name){
+        if(name.equals("1")){
+            return "是";
+        }else{
+            return "否";
+        }
+    }
+
+    public String[][] getData(String name) {
+        String attribute = searchMassage(name);
+        String sql = String.format("select user.user_name,institute.institute,form.time,form.hb,form.fr,form.ky," +
+                "form.tw from form,institute,user where form.institute_id=institute.id and form.user_id=user.id " +
+                "and form.attribute='%s'", attribute);
+        rs = db.selectSqlDate(sql);
+        ArrayList<ArrayList<String>> data = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                ArrayList<String> al = new ArrayList<>();
+                al.add(rs.getString("user_name"));
+                al.add(rs.getString("institute"));
+                al.add(rs.getString("time"));
+                al.add(getYesNo(rs.getString("hb")));
+                al.add(getYesNo(rs.getString("fr")));
+                al.add(getYesNo(rs.getString("ky")));
+                al.add(rs.getString("tw"));
+                data.add(al);
+            }
+            return ArrayListToArray(data);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/com/main/LeaderPanel.java
@@ -0,0 +1,70 @@
+package com.main;
+import com.db.LeaderDao;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LeaderPanel extends JFrame implements ActionListener {
+    private JButton reEnter;
+    private JPanel buttonPanel;
+    private JButton quit,out;
+    private LeaderDao td;
+    private JScrollPane sp;
+    private String name;
+    public LeaderPanel(String name) {
+        this.name = name;
+        init();
+        addActionListener();
+        this.setTitle(String.format(" %s 查看今日统计", name));
+        this.setSize(700, 500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        //this.add(reEnter, BorderLayout.NORTH);
+        this.add(buttonPanel, BorderLayout.SOUTH);
+        this.add(sp, BorderLayout.CENTER);
+    }
+
+    public void init() {
+        td = new LeaderDao();
+        buttonPanel = new JPanel();
+        reEnter = new JButton("刷新当前数据");
+        out = new JButton("导出");
+        quit = new JButton("退出");
+        buttonPanel.add(reEnter);
+        buttonPanel.add(quit);
+        buttonPanel.add(out);
+        tableData();
+    }
+
+    private void tableData() {
+        String[] titles = {"序号", "姓名", "学院", "时间", "是否湖北籍", "是否存在有发热", "近14天是否接触过感染者","体温"};
+        String[][] data = td.getData(name);
+        JTable table =new JTable(data,titles);
+        sp=new JScrollPane(table);
+        this.repaint();
+    }
+
+    private void addActionListener() {
+        reEnter.addActionListener(this);
+        out.addActionListener(this);
+        quit.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals(reEnter.getActionCommand())) {
+            tableData();
+        }else if (e.getActionCommand().equals(quit.getActionCommand())) {
+            System.exit(0);
+        }else if (e.getActionCommand().equals(out.getActionCommand())) {
+        	JOptionPane.showMessageDialog(null, "导出成功", "成功",JOptionPane.INFORMATION_MESSAGE);
+
+        }else{
+            JOptionPane.showMessageDialog(null, "导出失败", "失败",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
